f4e4f8fb0620ff559143dbe7979693e7635f4ce2
Ossiel <acevedoossiel@gmail.com>
2024-12-20 21:02:58 -0600

    chore: updating size canvas (#25)

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -26,7 +26,7 @@ const AppContent = () => {
           });
 
           if (response.ok) {
-            const data = await response.json();
+            //const data = await response.json();
           } else {
             Cookies.remove('authToken');
           }
